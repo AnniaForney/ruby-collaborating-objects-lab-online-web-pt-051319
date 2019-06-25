6ee5ae918d8404b7700deb42994c91a64a773637
Annia Forney <anniaforney@gmail.com>
2019-06-25 18:27:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,20 @@
 class Artist
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @songs = [] 
   end
 
   def add_song
   end
 
   def save
+    @@all
   end
 
   def self.find_or_create_by_name
-  end 
+  end
 end
